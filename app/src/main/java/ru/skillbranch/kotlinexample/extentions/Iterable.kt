2d836f05d58c3bfff9a1c91e43cc45a0d239d2eb
VaDikOo8 <vadikoo8.patron@gmail.com>
2020-03-15 17:47:37 +0300

    1 additional task was done

--- app/src/main/java/ru/skillbranch/kotlinexample/extentions/Iterable.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/extentions/Iterable.kt
@@ -1,2 +1,13 @@
 package ru.skillbranch.kotlinexample.extentions
 
+fun <T> List<T>.dropLastUntil(predicate: (T) -> Boolean): List<T> {
+    val result = arrayListOf<T>()
+    for (item in this) {
+        if (predicate(item)) {
+            return result
+        } else {
+            result.add(item)
+        }
+    }
+    return result
+}
