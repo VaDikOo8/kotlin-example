ee8fffb987c50931084a7539c94b98f9303939e7
VaDikOo8 <vadikoo8.patron@gmail.com>
2020-03-15 22:11:03 +0300

    hometask_2 was done

--- app/src/main/java/ru/skillbranch/kotlinexample/User.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/User.kt
@@ -26,14 +26,6 @@ class User private constructor(
     private var phone: String? = null
         set(value) {
             field = value?.replace("""[^+\d]""".toRegex(), "")
-                ?.also { it ->
-                    when {
-                        it.matches("""\+\d{11}""".toRegex()) -> it
-                        else -> throw IllegalArgumentException(
-                            "Enter a valid phone number starting with a + and containing 11 digits"
-                        )
-                    }
-                }
         }
 
     private var _login: String? = null
@@ -74,6 +66,26 @@ class User private constructor(
         println("Phone passwordHash is $passwordHash")
         accessCode = code
         sendAccessCodeToUser(rawPhone, code)
+    }
+
+    // For csv
+    constructor(
+        firstName: String,
+        lastName: String?,
+        email: String?,
+        rawPhone: String?,
+        saltImp: String,
+        passHash: String
+    ) : this(
+        firstName,
+        lastName,
+        email = email,
+        rawPhone = rawPhone,
+        meta = mapOf("src" to "csv")
+    ) {
+        println("Secondary csv constructor")
+        salt = saltImp
+        passwordHash = passHash
     }
 
     init {
@@ -143,11 +155,22 @@ class User private constructor(
             fullName: String,
             email: String? = null,
             password: String? = null,
-            phone: String? = null
+            phone: String? = null,
+            passHashWithSalt: String? = null
         ): User {
             val (firstName, lastName) = fullName.fullNameToPair()
+            val salt = passHashWithSalt?.split(":")?.first()
+            val passHash = passHashWithSalt?.split(":")?.last()
 
             return when {
+                !salt.isNullOrBlank() && !passHash.isNullOrBlank() -> User(
+                    firstName,
+                    lastName,
+                    email,
+                    phone,
+                    salt,
+                    passHash
+                )
                 !phone.isNullOrBlank() -> User(firstName, lastName, phone)
                 !email.isNullOrBlank() && !password.isNullOrBlank() -> User(
                     firstName,
--- app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
+++ app/src/main/java/ru/skillbranch/kotlinexample/UserHolder.kt
@@ -20,35 +20,67 @@ object UserHolder {
     fun registerUserByPhone(
         fullName: String,
         rawPhone: String
-    ): User = User.makeUser(fullName, phone = rawPhone)
-        .also { user ->
-            when {
-                !map.containsKey(user.login) -> map[user.login] = user
-                else -> throw IllegalArgumentException("A user with this phone already exists")
-            }
-        }
+    ): User =
+        if (rawPhone.isPhoneType()) {
+            User.makeUser(fullName, phone = rawPhone)
+                .also { user ->
+                    when {
+                        !map.containsKey(user.login) -> map[user.login] = user
+                        else -> throw IllegalArgumentException("A user with this phone already exists")
+                    }
+                }
+        } else throw IllegalArgumentException(
+            "Enter a valid phone number starting with a + and containing 11 digits"
+        )
 
     fun loginUser(login: String, password: String): String? =
-        if (login.trim().replace("""[^+\d]""".toRegex(), "").matches("""\+\d{11}""".toRegex()))
-            map[login.trim().replace("""[^+\d]""".toRegex(), "")]?.let {
-                if (it.checkPassword(password)) it.userInfo
-                else null
-            }
-        else map[login.trim()]?.let {
+        map[login.format()]?.let {
             if (it.checkPassword(password)) it.userInfo
             else null
         }
 
     fun requestAccessCode(login: String) {
-        map[login.trim().replace("""[^+\d]""".toRegex(), "")]?.also {
+        map[login.toPhoneType()]?.also {
             val accessCode = it.accessCode
             it.changePassword(accessCode!!, it.generateAccessCode())
         }
+    }
+
+    fun importUsers(list: List<String>): List<User> {
+        val userList = arrayListOf<User>()
+        for (user in list) {
+            val userData: List<String> = user.split(";").map { it.trim() }
+            userList.add(
+                User.makeUser(
+                    userData[0],
+                    email = userData[1].ifBlank { null },
+                    phone = userData[3].ifBlank { null },
+                    passHashWithSalt = userData[2].ifBlank { null }
+                ).also { user ->
+                    when {
+                        !map.containsKey(user.login) -> map[user.login] = user
+                        else -> throw IllegalArgumentException("A user with this data already exists")
+                    }
+                    println("userInfo: \n ${user.userInfo}")
+                }
+            )
+        }
+        return userList
     }
 
     @VisibleForTesting(otherwise = VisibleForTesting.NONE)
     fun clearHolder() {
         map.clear()
     }
+
+    private fun String.isPhoneType(): Boolean {
+        return this.toPhoneType().matches("""\+\d{11}""".toRegex())
+    }
+
+    private fun String.toPhoneType() = this.trim().replace("""[^+\d]""".toRegex(), "")
+
+    private fun String.format() =
+        if (this.isPhoneType()) this.toPhoneType()
+        else this.trim()
 }
 
